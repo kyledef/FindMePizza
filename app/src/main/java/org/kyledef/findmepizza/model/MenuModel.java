7a5a95e56f4e45eb3438bfca2a906d871b9ab3da
kyledef <kyle.e.defreitas@gmail.com>
2015-03-19 21:49:58 -0400

    added MenuModel and activity

---
+++ app/src/main/java/org/kyledef/findmepizza/model/MenuModel.java
@@ -0,0 +1,59 @@
+package org.kyledef.findmepizza.model;
+
+
+public class MenuModel {
+    String franchise;
+    String name;
+    String category;
+    String type;
+    String cost;
+
+
+    public MenuModel(String franchise, String name, String category, String type, String cost) {
+        this.franchise = franchise;
+        this.name = name;
+        this.category = category;
+        this.type = type;
+        this.cost = cost;
+    }
+
+    public String getFranchise() {
+        return franchise;
+    }
+
+    public void setFranchise(String franchise) {
+        this.franchise = franchise;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+}
---
+++ app/src/main/java/org/kyledef/findmepizza/ui/MenuList.java
@@ -0,0 +1,5 @@
+package org.kyledef.findmepizza.ui;
+
+public class MenuList extends BaseActivity {
+
+}
